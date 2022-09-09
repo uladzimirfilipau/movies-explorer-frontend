d3fb1ca68e2cd691c5131ccf1be0e51a2dd1c283
Vova <uladzimirfilipau@gmail.com>
2022-09-09 12:17:45 +0300

    add Movies

---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Movies.css';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+
+function Movies() {
+  return (
+    <main className='movies'>
+      <SearchForm />
+      <MoviesCardList />
+    </main>
+  );
+}
+
+export default Movies;
