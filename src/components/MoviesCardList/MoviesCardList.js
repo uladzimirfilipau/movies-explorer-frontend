2693a07b2e370032082caba5de68f12213948955
Vova <uladzimirfilipau@gmail.com>
2022-09-09 12:19:13 +0300

    add MoviesCardList

---
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './MoviesCardList.css';
+import MoviesCard from '../MoviesCard/MoviesCard';
+import { initialImages } from '../../utils/const';
+
+function MoviesCardList() {
+  const movieCard = initialImages.map(({ thumbnail }) => (
+    <MoviesCard
+      key={thumbnail.toString()}
+      nameRU={'33 слова о дизайне'}
+      duration={'1ч 47м'}
+      thumbnail={thumbnail}
+    />
+  ));
+  return (
+    <section className='movies__cards'>
+      <ul className='movies__list'>{movieCard}</ul>
+      <button className='movies__button'>Ещё</button>
+    </section>
+  );
+}
+
+export default MoviesCardList;
