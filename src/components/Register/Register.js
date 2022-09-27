f181f190281e13f9dd60cebae8e5aa617b17bae7
Vova <uladzimirfilipau@gmail.com>
2022-09-27 10:36:14 +0300

    upd Register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,78 +1,79 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Register.css';
+import useFormAndValidation from '../../hooks/useFormAndValidation';
+import { NAME } from '../../utils/consts';
 
 function Register({ onRegister }) {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  function handleUsernameChange(e) {
-    setUsername(e.target.value);
-  }
-
-  function handleEmailChange(e) {
-    setEmail(e.target.value);
-  }
-
-  function handlePasswordChange(e) {
-    setPassword(e.target.value);
-  }
+  const { values, handleChange, errors, isValid } = useFormAndValidation();
+  const [isSubmit, setIsSubmit] = useState(false);
+  const registerButtonClassName = `register__button ${
+    isValid ? 'register__button_enabled' : 'register__button_disabled'
+  }`;
 
   function handleSubmit(e) {
     e.preventDefault();
-    onRegister({ username, email, password });
+    onRegister(values);
+    setIsSubmit(true);
   }
 
   return (
-    <form className='form' onSubmit={handleSubmit}>
-      <Link to='/' className='form__logo' />
+    <form className='register' noValidate onSubmit={handleSubmit}>
+      <Link to='/' className='register__logo' />
 
-      <h2 className='form__title'>Добро пожаловать!</h2>
+      <h2 className='register__title'>Добро пожаловать!</h2>
 
-      <label className='form__label'>Имя</label>
+      <label className='register__label'>Имя</label>
       <input
-        className='form__input'
+        className='register__input'
         placeholder='Введите имя'
-        name='username'
+        name='name'
         type='text'
-        id='username'
-        value={username}
-        onChange={handleUsernameChange}
+        id='name'
+        value={values.name || ''}
+        onChange={handleChange}
+        minLength='2'
+        maxLength='30'
+        pattern={NAME}
         required
       />
+      <span className='register__input-error'>{errors.name}</span>
 
-      <label className='form__label'>E-mail</label>
+      <label className='register__label'>E-mail</label>
       <input
-        className='form__input'
+        className='register__input'
         placeholder='Введите e-mail'
         name='email'
         type='email'
         id='email'
-        value={email}
-        onChange={handleEmailChange}
+        value={values.email || ''}
+        onChange={handleChange}
         required
       />
+      <span className='register__input-error'>{errors.email}</span>
 
-      <label className='form__label'>Пароль</label>
+      <label className='register__label'>Пароль</label>
       <input
-        className='form__input'
+        className='register__input'
         placeholder='Введите пароль'
         name='password'
         type='password'
         id='password'
-        value={password}
-        onChange={handlePasswordChange}
+        value={values.password || ''}
+        onChange={handleChange}
+        minLength='8'
+        maxLength='30'
         required
       />
+      <span className='register__input-error'>{errors.password}</span>
 
-      <button type='submit' className='form__button form__button_register'>
+      <button className={registerButtonClassName} type='submit' disabled={!isValid || isSubmit}>
         Зарегистрироваться
       </button>
 
-      <p className='form__caption'>
+      <p className='register__caption'>
         Уже зарегистрированы?&nbsp;
-        <Link to='/signin' className='form__link'>
+        <Link to='/signin' className='register__link'>
           Войти
         </Link>
       </p>
