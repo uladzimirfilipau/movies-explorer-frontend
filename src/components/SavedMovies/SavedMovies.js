6f6eb802906c489423ca685ad1b34cd767dbd57b
Vova <uladzimirfilipau@gmail.com>
2022-09-27 10:44:09 +0300

    upd SavedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,13 +1,88 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+
 import './SavedMovies.css';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Preloader from '../../components/Preloader/Preloader';
 
-function SavedMovies() {
+function SavedMovies({ userMovies, handleDeleteMovie }) {
+  const [submitted, setSubmitted] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [findMovies, setFindMovies] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const [error, setError] = useState('');
+  const [shortMovies, setShortMovies] = useState([]);
+  const [found, setFound] = useState(true);
+
+  useEffect(() => {
+    setFindMovies(userMovies);
+  }, [userMovies]);
+
+  useEffect(() => {
+    if (submitted) {
+      const searchResults = userMovies.filter((movie) => {
+        const movieName = movie.nameRU.toLowerCase();
+        return movieName.includes(searchTerm.toLowerCase());
+      });
+
+      if (searchResults < 1) {
+        setFound(false);
+      } else {
+        setFindMovies(searchResults);
+        setFound(true);
+      }
+    }
+    setTimeout(() => setSubmitted(false), 1000);
+  }, [submitted, userMovies, searchTerm, setFindMovies]);
+
+  useEffect(() => {
+    setError('');
+  }, [searchTerm]);
+
+  useEffect(() => {
+    if (checked) {
+      const shortMovies = findMovies.filter((movie) => {
+        return movie.duration <= 40;
+      });
+
+      setShortMovies(shortMovies);
+    }
+  }, [checked, findMovies, setShortMovies]);
+
+  function handleChange(e) {
+    setSearchTerm(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    searchTerm ? setSubmitted(true) : setError('Нужно ввести ключевое слово');
+  }
+
+  function handleToogleCheck() {
+    setChecked(!checked);
+  }
+
   return (
     <main className='saved-movies'>
-      <SearchForm />
-      <MoviesCardList />
+      <SearchForm
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        searchTerm={searchTerm}
+        error={error}
+        checked={checked}
+        handleToogleCheck={handleToogleCheck}
+      />
+      {submitted ? (
+        <Preloader />
+      ) : found ? (
+        <MoviesCardList
+          movies={checked ? shortMovies : findMovies}
+          savedMovies={userMovies}
+          handleDeleteMovie={handleDeleteMovie}
+        />
+      ) : (
+        <p className='movies__not-found'>Ничего не найдено</p>
+      )}
     </main>
   );
 }
