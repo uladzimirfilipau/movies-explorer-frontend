3bcc4bddea0f8653fcca4e1f14b5e3ae2517ff19
Vova <uladzimirfilipau@gmail.com>
2022-09-27 10:40:45 +0300

    upd PageNotFound

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+
 import './PageNotFound.css';
 
 function PageNotFound() {
+  let history = useHistory();
+
+  const goToPreviousPath = () => {
+    history.goBack();
+  };
+
   return (
     <section className='not-found'>
       <h2 className='not-found__title'>404</h2>
       <p className='not-found__subtitle'>Страница не найдена</p>
-      <Link to='/' className='not-found__link'>
+
+      <Link className='not-found__link' onClick={goToPreviousPath}>
         Назад
       </Link>
     </section>
