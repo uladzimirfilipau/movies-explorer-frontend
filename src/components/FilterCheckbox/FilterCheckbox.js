3538be417eb98e8b9101d146b7b7b4c67cdf4998
Vova <uladzimirfilipau@gmail.com>
2022-09-27 10:43:47 +0300

    upd FilterCheckbox

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './FilterCheckbox.css';
 
-function FilterCheckbox() {
+function FilterCheckbox({ checked, handleToogleCheck }) {
   return (
     <form className='checkbox'>
-      <input className='checkbox__input' type='checkbox' id='checkbox' name='checkbox' />
-      <label className='checkbox__label'>Короткометражки</label>
+      <input
+        className='checkbox__input'
+        type='checkbox'
+        id='checkbox'
+        name='checkbox'
+        checked={checked}
+        onChange={handleToogleCheck}
+        aria-label='Найти короткометражки'
+      />
+      <label className='checkbox__label' htmlFor='checkbox'>
+        Короткометражки
+      </label>
     </form>
   );
 }
