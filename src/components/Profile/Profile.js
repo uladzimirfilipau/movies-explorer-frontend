d9996e5294492e182dbe0b555e38a2945b158529
Vova <uladzimirfilipau@gmail.com>
2022-09-27 10:36:53 +0300

    upd Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,77 +1,88 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Profile.css';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import useFormAndValidation from '../../hooks/useFormAndValidation';
 
-function Profile() {
-  const [username, setUsername] = useState('Владимир');
-  const [email, setEmail] = useState('pochta@yandex.ru');
-  const [isEdit, setIsEdit] = useState(false);
+function Profile({ handleUpdateUser, handleSignOut }) {
+  const { values, handleChange, isValid, resetForm } = useFormAndValidation();
+  const currentUser = useContext(CurrentUserContext);
+  let newData = currentUser.name !== values.name || currentUser.email !== values.email;
+  const saveButtonClassName = `profile__save-button ${
+    newData && isValid ? 'profile__save-button_enabled' : 'profile__save-button_disabled'
+  }`;
+  const editButtonClassName = `profile__edit-button ${
+    (!newData || !isValid) && 'profile__edit-button_disabled'
+  }`;
+  const [isSaveButton, setIsSaveButton] = useState(false);
 
-  function handleUsernameChange(e) {
-    setUsername(e.target.value);
+  useEffect(() => {
+    resetForm(currentUser, {}, true);
+  }, [resetForm, currentUser]);
+
+  function handleEdit() {
+    if (isValid && newData) {
+      setIsSaveButton(true);
+    }
   }
 
-  function handleEmailChange(e) {
-    setEmail(e.target.value);
-  }
-
-  function handleEditClick() {
-    setIsEdit(true);
-  }
-
-  function handleSaveClick() {
-    setIsEdit(false);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (isValid && newData) {
+      handleUpdateUser(values);
+      setIsSaveButton(false);
+    }
   }
 
   return (
-    <section className='profile'>
-      <h2 className='profile__title'>Привет, Владимир!</h2>
+    <form className='profile' noValidate onSubmit={handleSubmit}>
+      <h2 className='profile__title'>Привет, {currentUser.name}!</h2>
 
-      <form className='profile__form'>
-        <fieldset className='profile__fildset'>
-          <label className='profile__label'>Имя</label>
-          <input
-            className='profile__input'
-            placeholder='Введите имя'
-            name='username'
-            type='text'
-            id='username'
-            value={username}
-            onChange={handleUsernameChange}
-            required
-          />
-        </fieldset>
+      <fieldset className='profile__fildset'>
+        <label className='profile__label'>Имя</label>
+        <input
+          className='profile__input'
+          placeholder='Введите имя'
+          name='name'
+          type='text'
+          id='name'
+          value={values.name || ''}
+          onChange={handleChange}
+          minLength='2'
+          maxLength='30'
+          required
+        />
+      </fieldset>
 
-        <fieldset className='profile__fildset'>
-          <label className='profile__label'>E-mail</label>
-          <input
-            className='profile__input'
-            placeholder='Введите e-mail'
-            name='email'
-            type='email'
-            id='email'
-            value={email}
-            onChange={handleEmailChange}
-            required
-          ></input>
-        </fieldset>
-      </form>
+      <fieldset className='profile__fildset'>
+        <label className='profile__label'>E-mail</label>
+        <input
+          className='profile__input'
+          placeholder='Введите e-mail'
+          name='email'
+          type='email'
+          id='email'
+          value={values.email || ''}
+          onChange={handleChange}
+          required
+        />
+      </fieldset>
 
-      {isEdit ? (
-        <button className='profile__save-button' type='submit' onClick={handleSaveClick}>
+      {isSaveButton && newData ? (
+        <button className={saveButtonClassName} type='submit'>
           Сохранить
         </button>
       ) : (
         <>
-          <button className='profile__edit-button' type='button' onClick={handleEditClick}>
+          <button className={editButtonClassName} type='button' onClick={handleEdit}>
             Редактировать
           </button>
-          <Link to='/' className='profile__logout-link'>
+          <Link to='/' className='profile__logout-link' onClick={handleSignOut}>
             Выйти из аккаунта
           </Link>
         </>
       )}
-    </section>
+    </form>
   );
 }
 
