78168ee789915f609c1dfdbe8233f275f2bb2a35
Vova <uladzimirfilipau@gmail.com>
2023-03-06 23:51:17 +0300

    refactor: change MAIN_URL link

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -4,7 +4,7 @@ export const API_URL = 'https://api.nomoreparties.co';
 export let MAIN_URL = '';
 const { NODE_ENV } = process.env;
 if (NODE_ENV === 'production') {
-  MAIN_URL = 'https://api.moviesexplorer.vova.nomoredomains.sbs';
+  MAIN_URL = 'https://api-movie-explorer-vova.herokuapp.com';
 } else {
   MAIN_URL = 'http://localhost:3001';
 }
@@ -46,4 +46,3 @@ export const WRONG_NAME = 'Используйте только латиницу,
 export const WRONG_EMAIL = 'Некорректный email';
 
 export const KEYWORD_ERROR = 'Нужно ввести ключевое слово';
-
